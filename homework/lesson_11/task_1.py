57c6b64aae040ccd2c547265717ed0d75c19fe83
dante <master.neo15@gmail.com>
2021-05-29 14:44:49 +0300

    Fucking commit

--- homework/lesson_11/task_1.py
+++ homework/lesson_11/task_1.py
@@ -11,26 +11,38 @@ attribute, while salary should only be available to the teacher.
 class Person:
 
     def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+        self._first_name = first_name
+        self._last_name = last_name
+        self._age = age
 
     def show_inf(self):
-        return(f"Person's name is {self.first_name} {self.last_name} and it is {self.age} years old!")
+        return(f"Person's name is {self._first_name} {self._last_name} and he is {self._age} years old!")
 
 class Student(Person):
 
-    def __init__(self, course, marks):
-        super.__init__()
-        self.marks = marks
+    def __init__(self, first_name, last_name, age, course, course_name):
+        super().__init__(first_name, last_name, age)
+        self.course_name = course_name
         self.course = course
 
     def show_student_info(self):
-        return(f"Student name is {}. He lerarning at {} ans has {}")
+        return(f"He learning at {self.course} course on {self.course_name}")
+
+class Teacher(Person):
+
+    def __init__(self, first_name, last_name, age, subject, salary):
+        super().__init__(first_name, last_name, age)
+        self.salary = salary
+        self.subject = subject
+
+    def show_teacher_info(self):
+        return(f"He is teaching {self.subject} and has salary {self.salary} per month")
 
 
-dante = Person("Dante", "Alighieri", 26)
-dante_student = Student('IB', [5,5,5,5,5,5])
+
+dante = Student("Dante", "Alighieri", 26, '5', "IB")
+sparda = Teacher("Sparda", "Alighieri", 26, "History", 1000)
 print(dante.show_inf())
-print(dante_student.show_student_info())
-
+print(dante.show_student_info())
+print(sparda.show_inf())
+print(sparda.show_teacher_info())
